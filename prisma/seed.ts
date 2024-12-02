f0300c2518c796f16358255f373ec2ae562aa152
LaserGods <jordan@hdmsarch.com>
2024-12-01 19:14:02 -0600

    database patterns, replace AuthUser with User from prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,18 @@
 import { PrismaClient } from "@prisma/client";
+import { hashPassword } from "@/features/password/utils/hash-and-verify";
 
 const prisma = new PrismaClient();
+
+const users = [
+  {
+    username: "admin",
+    email: "admin@admin.com",
+  },
+  {
+    username: "user",
+    email: "jordan_hicks@icloud.com",
+  },
+];
 
 const tickets = [
   {
@@ -30,10 +42,23 @@ const seed = async () => {
   const t0 = performance.now();
   console.log("DB Seed: Started ...");
 
+  await prisma.user.deleteMany();
   await prisma.ticket.deleteMany();
 
+  const passwordHash = await hashPassword("geheimnis");
+
+  const dbUsers = await prisma.user.createManyAndReturn({
+    data: users.map((user) => ({
+      ...user,
+      passwordHash,
+    })),
+  });
+
   await prisma.ticket.createMany({
-    data: tickets,
+    data: tickets.map((ticket) => ({
+      ...ticket,
+      userId: dbUsers[0].id,
+    })),
   });
 
   const t1 = performance.now();
