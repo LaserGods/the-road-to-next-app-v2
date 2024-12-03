a36b75164030ef2ddcfb049fe77295e6f025a327
LaserGods <jordan@hdmsarch.com>
2024-12-02 22:19:20 -0600

    add account dropdown, sidebar

--- src/app/(authenticated)/tickets/[ticketId]/edit/page.tsx
+++ src/app/(authenticated)/tickets/[ticketId]/edit/page.tsx
@@ -1,9 +1,12 @@
+import { Separator } from "@radix-ui/react-separator";
 import { notFound } from "next/navigation";
+import { Breadcrumbs } from "@/components/breadcrumbs";
 import { CardCompact } from "@/components/card-compact";
 import { getAuth } from "@/features/auth/queries/get-auth";
 import { isOwner } from "@/features/auth/utils/is-owner";
 import { TicketUpsertForm } from "@/features/ticket/components/ticket-upsert-form";
 import { getTicket } from "@/features/ticket/queries/get-ticket";
+import { homePath, ticketPath } from "@/paths";
 
 type TicketEditPageProps = {
   params: Promise<{
@@ -24,13 +27,24 @@ const TicketEditPage = async ({ params }: TicketEditPageProps) => {
   }
 
   return (
-    <div className="flex flex-1 flex-col items-center justify-center">
-      <CardCompact
-        title="Edit Ticket"
-        description="Edit an existing ticket"
-        className="w-full max-w-[420px] animate-fade-from-top"
-        content={<TicketUpsertForm ticket={ticket} />}
+    <div className="flex flex-1 flex-col gap-y-8">
+      <Breadcrumbs
+        breadcrumbs={[
+          { title: "Tickets", href: homePath() },
+          { title: ticket.title, href: ticketPath(ticket.id) },
+          { title: "Edit" },
+        ]}
       />
+
+      <Separator />
+      <div className="flex flex-1 flex-col items-center justify-center">
+        <CardCompact
+          title="Edit Ticket"
+          description="Edit an existing ticket"
+          className="w-full max-w-[420px] animate-fade-from-top"
+          content={<TicketUpsertForm ticket={ticket} />}
+        />
+      </div>
     </div>
   );
 };
--- src/components/sidebar/components/sidebar.tsx
+++ src/components/sidebar/components/sidebar.tsx
@@ -1,12 +1,22 @@
 "use client";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { useAuth } from "@/features/auth/hooks/use-auth";
 import { cn } from "@/lib/utils";
+import { signInPath, singUpPath } from "@/paths";
+import { getActivePath } from "@/utils/get-active-path";
 import { navItems } from "../constants";
 import { SidebarItem } from "./sidebar-item";
 
 const Sidebar = () => {
   const { user, isFetched } = useAuth();
+  const pathName = usePathname();
+
+  const { activeIndex } = getActivePath(
+    pathName,
+    navItems.map((item) => item.href),
+    [singUpPath(), signInPath()],
+  );
 
   const [isTransition, setTransition] = useState(false);
   const [isOpen, setOpen] = useState(false);
@@ -22,7 +32,7 @@ const Sidebar = () => {
   return (
     <nav
       className={cn(
-        "h-screen border-r pt-24",
+        "h-screen border-r pt-24 delay-200",
         isTransition && "duration-200",
         isOpen ? "w-[78px] md:w-60" : "w-[78px]",
       )}
@@ -31,10 +41,11 @@ const Sidebar = () => {
     >
       <div className="px-3 py-2">
         <nav className="space-y-2">
-          {navItems.map((navItem) => (
+          {navItems.map((navItem, index) => (
             <SidebarItem
               key={navItem.title}
               isOpen={isOpen}
+              isActive={activeIndex === index}
               navItem={navItem}
             />
           ))}
