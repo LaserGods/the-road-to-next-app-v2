2d48ca232497163f9d89a6c63317e8d5cecf0072
LaserGods <jordan@hdmsarch.com>
2024-12-22 23:06:06 -0600

    add comment table, fix type errors

--- src/components/placeholder.tsx
+++ src/components/placeholder.tsx
@@ -3,8 +3,8 @@ import { cloneElement } from "react";
 
 type PlaceholderProps = {
   label: string;
-  icon?: React.ReactElement;
-  button?: React.ReactElement;
+  icon?: React.ReactElement<React.SVGProps<SVGSVGElement>, "svg">;
+  button?: React.ReactElement<React.SVGProps<SVGSVGElement>, "svg">;
 };
 
 const Placeholder = ({
