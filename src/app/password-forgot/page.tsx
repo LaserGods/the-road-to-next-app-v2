fc59739283dab7a6294c780f122d87ebe5e6b847
LaserGods <jordan@hdmsarch.com>
2025-02-03 14:13:10 -0600

    Forgot password (public page)

--- src/app/password-forgot/page.tsx
+++ src/app/password-forgot/page.tsx
@@ -1,5 +1,17 @@
+import { CardCompact } from "@/components/card-compact";
+import { PasswordForgotForm } from "@/features/password/components/password-forgot-form";
+
 const PasswordForgotPage = () => {
-  return <div>PasswordForgotPage</div>;
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <CardCompact
+        title="Forgot Password"
+        description="Enter your email address to reset your password."
+        className="w-full max-w-[420px] animate-fade-from-top"
+        content={<PasswordForgotForm />}
+      />
+    </div>
+  );
 };
 
 export default PasswordForgotPage;
