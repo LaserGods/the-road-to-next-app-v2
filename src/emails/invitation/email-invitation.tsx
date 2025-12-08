0e49dfa94b1b660ef109c39176506df97ea3d926
LaserGods <jordan@hdmsarch.com>
2025-12-08 15:42:56 -0600

    Invitation email

---
+++ src/emails/invitation/email-invitation.tsx
@@ -0,0 +1,60 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Html,
+  Preview,
+  Section,
+  Tailwind,
+  Text,
+} from "@react-email/components";
+
+type EmailInvitationProps = {
+  fromUser: string;
+  fromOrganization: string;
+  url: string;
+};
+
+const EmailInvitation = ({
+  fromUser,
+  fromOrganization,
+  url,
+}: EmailInvitationProps) => {
+  return (
+    <Html>
+      <Head />
+      <Tailwind>
+        <Body className="m-8 text-center font-sans">
+          <Preview>
+            Invitation to join {fromOrganization} on TicketBounty
+          </Preview>
+          <Container>
+            <Section>
+              <Text>
+                Hello there, {fromUser} invited you to join {fromOrganization}.
+                Click the link below to accept the invitation.
+              </Text>
+            </Section>
+            <Section>
+              <Button
+                href={url}
+                className="m-2 rounded bg-black p-2 text-white"
+              >
+                Accept Invitation
+              </Button>
+            </Section>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
+
+EmailInvitation.PreviewProps = {
+  fromUser: "Robin Wieruch",
+  fromOrganization: "TicketBounty",
+  url: "http://localhost:3000/email-invitation/abc123",
+} as EmailInvitationProps;
+
+export default EmailInvitation;
